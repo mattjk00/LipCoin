fb1810c93b7670c52ed5d80dd3d45f17a0558b44
Matthew K <mattjk@outlook.com>
2021-03-08 23:14:15 -0500

    Ok did away with some of the namespaces.

--- src/nft.ts
+++ src/nft.ts
@@ -1,22 +1,21 @@
-export namespace NFT {
-    /**
-     * The type of media a token could have
-     */
-    export enum MediaType {
-        Image,
-        Audio,
-        Text
-    }
-    
-    /**
-     * A token that can be created and then traded on the blockchain.
-     */
-    export interface Token {
-        type:MediaType,
-        hash:Array<number>,
-        signature:Array<number>,
-        creator:Array<number>,
-        url:String,
-        price:number
-    }
+/**
+ * The type of media a token could have
+ */
+export enum MediaType {
+    Image,
+    Audio,
+    Text
 }
+
+/**
+ * A token that can be created and then traded on the blockchain.
+ */
+export interface Token {
+    type:MediaType,
+    hash:Array<number>,
+    signature:Array<number>,
+    creator:Array<number>,
+    url:String,
+    price:number
+}
+
--- src/transaction.ts
+++ src/transaction.ts
@@ -3,33 +3,29 @@ import {Arr} from "./util"
 type TValue = number|Token;
 
 /**
- * The namespace contains structures and functions relevant to Transactions
+ * Represents a transaction that can be saved in the blockchain
  */
-export namespace Transactions {
-    /**
-     * Represents a transaction that can be saved in the blockchain
-     */
-    export interface Transaction {
-        sender:Array<number>,
-        recipient:Array<number>,
-        signature:Array<number>,
-        value:TValue
-    }
-
-    /**
-     * Constructs an UNSIGNED transaction with given information.
-     * @param send Sender of the transaction
-     * @param recipient Recipient of the transaction
-     * @param value Value being transferred
-     */
-    export function newTransaction(send:Array<number>, recipient:Array<number>, value:TValue) {
-        return {
-            sender:send,
-            recipient:recipient,
-            signature:Arr.empty<number>(32, 0),
-            value:value
-        } as Transaction;
-    }
+export interface Transaction {
+    sender:Array<number>,
+    recipient:Array<number>,
+    signature:Array<number>,
+    value:TValue
 }
+
+/**
+ * Constructs an UNSIGNED transaction with given information.
+ * @param send Sender of the transaction
+ * @param recipient Recipient of the transaction
+ * @param value Value being transferred
+ */
+export function newTransaction(send:Array<number>, recipient:Array<number>, value:TValue) {
+    return {
+        sender:send,
+        recipient:recipient,
+        signature:Arr.empty<number>(32, 0),
+        value:value
+    } as Transaction;
+}
+
 
 //export function signTransaction(t:Transaction, )
